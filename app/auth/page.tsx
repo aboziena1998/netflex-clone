82d4043dad216777a6c5d3a443fbf9376ec43e79
abozienacoding <87572781+aboziena1998@users.noreply.github.com>
2023-08-01 03:24:41 +0300

    first commit

---
+++ app/auth/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const AUth = () => {
+  return (
+    <div className="relative w-full h-full bg-hero bg-no-repeat bg-center bg-fixed bg-cover"></div>
+  );
+};
+
+export default AUth;
