119f503edf96e2eb0fe0b4d739535006835f0631
abozienacoding <87572781+aboziena1998@users.noreply.github.com>
2023-08-07 20:37:18 +0300

    another attempt to fix prismadb type problem

--- lib/prismadb.ts
+++ lib/prismadb.ts
@@ -1,6 +1,14 @@
 import { PrismaClient } from '@prisma/client';
 
-const client = (global as any).prismadb || new PrismaClient();
+declare global {
+  namespace NodeJS {
+    interface Global {
+      prismadb?: PrismaClient;
+    }
+  }
+}
+
+const client = (global as any)?.prismadb || new PrismaClient();
 if (process.env.NODE_ENV !== 'production') (global as any).prismadb = client;
 
 export default client;
